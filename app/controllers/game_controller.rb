862202b12668fe54075e9d6dc13e8efd4f711732
set830 <SET@Shawns-iMac.local>
2015-10-22 21:18:20 -0500

    Hw Complete!

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,34 +1,83 @@
 class GameController < ApplicationController
 
   def rock
-    @YouPlayed = "/assets/images/paper.png"
-    @YouPlayedTitle = "Paper"
-    @labelcolor = "label label-warning"
-    @YouSomething = "You Won!"
-    @computerPlayed = "/assets/images/paper.png"
-    @computerPlayedTitle = "Paper"
+    @YouPlayed = "/assets/images/rock.png"
+    @YouPlayedTitle = "Rock"
 
-
+    num = rand(3)
+    if num == 0
+      @computerPlayed = "/assets/images/rock.png"
+      @computerPlayedTitle = "Rock"
+      @labelcolor = "label label-warning"
+      @YouSomething = "You Tied!"
+    elsif num == 1
+      computer = "Paper"
+      @computerPlayed = "/assets/images/paper.png"
+      @computerPlayedTitle = "Paper"
+      @labelcolor = "label label-danger"
+      @YouSomething = "You Lost!"
+    elsif num == 2
+      computer = "Scissors"
+      @computerPlayed = "/assets/images/scissors.png"
+      @computerPlayedTitle = "Scissors"
+      @labelcolor = "label label-success"
+      @YouSomething = "You Won!"
+    end
 
     render ('rock.html.erb')
   end
+
+
   def paper
     @YouPlayed = "/assets/images/paper.png"
     @YouPlayedTitle = "Paper"
-    @labelcolor = "label label-warning"
-    @YouSomething = "You Won!"
-    @computerPlayed = "/assets/images/paper.png"
-    @computerPlayedTitle = "Paper"
+
+    num = rand(3)
+    if num == 0
+      @computerPlayed = "/assets/images/rock.png"
+      @computerPlayedTitle = "Rock"
+      @labelcolor = "label label-success"
+      @YouSomething = "You Won!"
+    elsif num == 1
+      computer = "Paper"
+      @computerPlayed = "/assets/images/paper.png"
+      @computerPlayedTitle = "Paper"
+      @labelcolor = "label label-warning"
+      @YouSomething = "You Tied!"
+    elsif num == 2
+      computer = "Scissors"
+      @computerPlayed = "/assets/images/scissors.png"
+      @computerPlayedTitle = "Scissors"
+      @labelcolor = "label label-danger"
+      @YouSomething = "You Lost!"
+
     render ('paper.html.erb')
   end
 
   def scissors
-    @YouPlayed = "/assets/images/paper.png"
-    @YouPlayedTitle = "Paper"
-    @labelcolor = "label label-warning"
-    @YouSomething = "You Won!"
-    @computerPlayed = "/assets/images/paper.png"
-    @computerPlayedTitle = "Paper"
+    @YouPlayed = "/assets/images/scissors.png"
+    @YouPlayedTitle = "Scissors"
+
+    num = rand(3)
+    if num == 0
+      @computerPlayed = "/assets/images/rock.png"
+      @computerPlayedTitle = "Rock"
+      @labelcolor = "label label-danger"
+      @YouSomething = "You Lost!"
+    elsif num == 1
+      computer = "Paper"
+      @computerPlayed = "/assets/images/paper.png"
+      @computerPlayedTitle = "Paper"
+      @labelcolor = "label label-success"
+      @YouSomething = "You Won!"
+    elsif num == 2
+      computer = "Scissors"
+      @computerPlayed = "/assets/images/scissors.png"
+      @computerPlayedTitle = "Scissors"
+      @labelcolor = "label label-warning"
+      @YouSomething = "You Tied!"
+
     render ('scissors.html.erb')
   end
+
 end
